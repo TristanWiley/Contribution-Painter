b3a57e062725534a477c8a98c7faf8779cf5d8e6
Tristan Wiley <tristan@tristanwiley.com>
2017-01-03 18:22:19 -0500

    First commit

---
+++ popup.js
@@ -0,0 +1,18 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+
+function click(e) {
+	var el = document.getElementById('colorCode');
+	localStorage["curColor"] = e.target.value;
+	alert(localStorage["curColor"]);
+  // window.close();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  var divs = document.querySelectorAll('div');
+  for (var i = 0; i < divs.length; i++) {
+    divs[i].addEventListener('click', click);
+  }
+});

b0721467680b317f44126280b7ed9f5f28d05c8b
Tristan Wiley <tristan@tristanwiley.com>
2017-01-06 17:06:32 -0500

    Added custom color box, #2

--- popup.js
+++ popup.js
@@ -1,13 +1,36 @@
 function click(e) {
     var el = document.getElementById('colorCode');
     chrome.storage.sync.set({ 'curColor': e.target.getAttribute('data-id') }, function() {
-        console.log('Color changed');
+        changeColor(e.target.getAttribute('data-id'));
     });
 }
 
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+    chrome.storage.sync.get('curColor', function(item) {
+        var colorInput = document.getElementById('colorInput');
+        colorInput.value = item.curColor;
+    });
+});
+
+
 document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('colorInput').addEventListener('input', function() {
+        changeColor(colorInput.value);
+    });
+    
+    chrome.storage.sync.get('curColor', function(item) {
+        var colorInput = document.getElementById('colorInput');
+        colorInput.value = item.curColor;
+    });
+
     var divs = document.querySelectorAll('div');
     for (var i = 0; i < divs.length; i++) {
         divs[i].addEventListener('click', click);
     }
 });
+
+function changeColor(color) {
+    chrome.storage.sync.set({ 'curColor': color }, function() {
+        console.log('Color changed');
+    });
+}
